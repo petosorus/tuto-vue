6e26698e2be27ae22227d6704130c946ada5e13c
petosorus <petosorus@hotmail.fr>
2023-11-09 14:58:43 +0100

    Gutting example out

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Exercice1 from '../views/Exercice1.vue'
+import Exercice2 from '../views/Exercice2.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +8,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Exercice1
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/exercice2',
+      name: 'exercice2',
+      component: Exercice2
     }
   ]
 })
---
+++ src/views/Exercice2.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <h1>Exercice 2</h1>
+  </main>
+</template>
